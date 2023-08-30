f14358f15bf77239b0d7854624166b7c353cb216
Anastasia Klochko <yolondy@Anastasias-MacBook-Pro.local>
2023-08-30 16:54:30 +1000

    Решена задача 24

--- ex22.py
+++ ex22.py
@@ -1,17 +1,20 @@
+'''Задача 22: Даны два неупорядоченных набора целых чисел (может быть, с повторениями). 
+    Выдать без повторений в порядке возрастания все те числа, которые встречаются в обоих наборах.
+    Пользователь вводит 2 числа. n — кол-во элементов первого множества. m — кол-во элементов второго множества. 
+    Затем пользователь вводит сами элементы множеств.'''
+
 print("Enter n: ")
 n = int(input())
 print('Fill list with n elements: ')
 listn = [int(input()) for i in range(n)]
-
 setn = set(listn)
 
 print("Enter m: ")
 m = int(input())
 print('Fill list with m elements: ')
 listm = [int(input()) for i in range(m)]
-
 setm = set(listm)
 
-generic = set.intersection(setn,setm)
+generated = set.intersection(setn,setm)
 
-print(sorted(generic))
+print(sorted(generated))
